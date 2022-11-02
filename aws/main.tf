c72ea8266d94d350af944c388d56f63aa1a2cbce
ExperiencedDevOps <service@contrasting.org>
2022-11-02 16:47:47 -0500

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -10,13 +10,3 @@ terraform {
 provider "aws" {
   region     = var.region
 }
-
-resource "aws_s3_bucket" "test" {
-  bucket = "${var.bucket_name}"
-  acl    = "private"
-
-  tags = {
-    Name        = "Test Bucket"
-    Environment = "Dev"
-  }
-}
