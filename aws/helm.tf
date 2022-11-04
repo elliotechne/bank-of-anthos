46b31cd4e1a1b650a47380f8ffcf188c0443d896
Brian Adams <service@contrasting.org>
2022-11-04 17:36:09 -0500

    feat: added clusterissuer helm chart

---
+++ aws/helm.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "cluster-issuer" {
+  provider  = helm.cinema
+  name      = "cluster-issuer"
+  chart     = "charts/cluster-issuer"
+  namespace = "kube-system"
+  depends_on = [
+    module.eks
+  ]
+  set_sensitive {
+    name  = "zerossl_email"
+    value = var.zerossl_email
+  }
+  set_sensitive {
+    name  = "zerossl-eab-hmac-key"
+    value = var.zerossl_eab_hmac_key
+  }
+  set_sensitive {
+    name  = "zerossl-eab-key-id"
+    value = var.zerossl_eab_key_id
+  }
+}
