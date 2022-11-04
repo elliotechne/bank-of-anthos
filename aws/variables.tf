53d9d12357031858417c5b3b90fc7b493017f183
Brian Adams <service@contrasting.org>
2022-11-04 16:21:23 -0500

    feat: added cluster-issuer chart and vars

--- aws/variables.tf
+++ aws/variables.tf
@@ -2,8 +2,13 @@ variable "region" {
     description = "The region where to provision resources"
     type = string
 }
-variable "bucket_name" {
-    description = "The name of S3 bucket"
-    default = "wayofthesys-test-bucket"
+
+variable "zerossl_eab_key_id" {
+    description = "ZeroSSL eab key id"
+    type = string
+}
+
+variable "zerossl_eab_hmac_key" {
+    description = "ZeroSSL eab hmac key"
     type = string
 }
