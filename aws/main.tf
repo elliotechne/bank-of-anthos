cf8cc66bcb659d51523b7bb23dc3cc51319eac0f
ExperiencedDevOps <service@contrasting.org>
2022-11-04 13:31:20 -0500

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_main.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
