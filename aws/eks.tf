bb95a7f9ad6a7686389c7537421f0c4aa7b6abae
Brian Adams <service@contrasting.org>
2022-11-05 15:11:59 -0500

    fix: add sg to eks node group

--- aws/eks.tf
+++ aws/eks.tf
@@ -44,6 +44,17 @@ module "eks" {
   # aws-auth configmap
   manage_aws_auth_configmap = true
 
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
+
   node_security_group_additional_rules = {
     ingress_self_all = {
       description = "Node to node all ports/protocols"
