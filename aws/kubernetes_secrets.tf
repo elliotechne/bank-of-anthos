04b1d1729452510acb034540d1bd8710a31d10cd
Brian Adams <service@contrasting.org>
2022-11-06 13:35:07 -0600

    feat: added tls secrets for domain and argo

--- aws/kubernetes_secrets.tf
+++ aws/kubernetes_secrets.tf
@@ -27,3 +27,45 @@ resource "kubernetes_secret" "zerossl_eab_key_id" {
 
   type = "kubernetes.io/opaque"
 }
+
+resource "kubernetes_secret" "argocd-tls" {
+  provider   = kubernetes.cinema
+  depends_on = [digitalocean_kubernetes_cluster.cinema, helm_release.external-dns, kubernetes_namespace.cinema]
+  metadata {
+    name = "argocd-tls"
+    namespace = "istio-system"
+  }
+  type = "tls"
+  data = {
+    "tls.crt" = tls_locally_signed_cert.cert.cert_pem
+    "tls.key" = tls_private_key.key.private_key_pem
+  }
+
+  lifecycle {
+    ignore_changes = [
+      data,
+      metadata
+    ]
+  }
+}
+
+resource "kubernetes_secret" "wayofthesys-tls" {
+  provider   = kubernetes
+  depends_on = [digitalocean_kubernetes_cluster.cinema, helm_release.external-dns, kubernetes_namespace.cinema]
+  metadata {
+    name      = "${replace(var.domain_name[0], ".", "-")}-tls"
+    namespace = "istio-system"
+  }
+  type = "tls"
+  data = {
+    "tls.crt" = tls_locally_signed_cert.cert.cert_pem
+    "tls.key" = tls_private_key.key.private_key_pem
+  }
+
+  lifecycle {
+    ignore_changes = [
+      data,
+      metadata
+    ]
+  }
+}
