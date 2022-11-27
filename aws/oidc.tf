ba78d1bcddb39cbf59b8d529be692b119464757c
Brian Adams <service@contrasting.org>
2022-11-27 15:30:33 -0600

    fix: add oidc provider

--- aws/oidc.tf
+++ aws/oidc.tf
@@ -2,5 +2,5 @@ resource "aws_iam_openid_connect_provider" "eks" {
   depends_on      = [module.eks]
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = []
-  url             = module.eks.oidc_provider_arn
+  url             = module.eks.oidc_provider_url
 }
