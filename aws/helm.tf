3717051688dd71da081963d7f70476d8350b740e
Brian Adams <service@contrasting.org>
2022-11-27 16:55:42 -0600

    fix: add oidc provider

--- aws/helm.tf
+++ aws/helm.tf
@@ -85,6 +85,7 @@ resource "helm_release" "aws-load-balancer-controller" {
 
 }
 
+/*
 resource "helm_release" "crossplane-config" {
   provider  = helm
   name      = "crossplane-config"
@@ -100,6 +101,7 @@ resource "helm_release" "crossplane-config" {
     value = var.github_pat
   }
 }
+*/
 
 resource "helm_release" "crossplane" {
   provider   = helm
