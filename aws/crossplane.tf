d4e9553e2d63707613d2711202f6826d7bd6004d
Brian Adams <service@contrasting.org>
2022-11-28 14:02:58 -0600

    feat: added crossplane IRSA

--- aws/crossplane.tf
+++ aws/crossplane.tf
@@ -5,8 +5,8 @@ module "aws_provider_irsa" {
   kubernetes_namespace              = "crossplane-system" 
   kubernetes_service_account        = "crossplane-config"
   irsa_iam_policies                 = concat([aws_iam_policy.aws_provider[0].arn], var.aws_provider.additional_irsa_policies)
-  irsa_iam_role_path                = var.addon_context.irsa_iam_role_path
-  irsa_iam_permissions_boundary     = var.addon_context.irsa_iam_permissions_boundary
+  irsa_iam_role_path                = var.irsa_iam_role_path
+  irsa_iam_permissions_boundary     = var.irsa_iam_permissions_boundary
   eks_cluster_id                    = module.eks.cluster_id 
   eks_oidc_provider_arn             = "https://${ module.eks.oidc_provider }"
   depends_on                        = [helm_release.crossplane-config]
