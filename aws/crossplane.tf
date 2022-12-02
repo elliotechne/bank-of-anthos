048f0a504e5f5441ad6b7625e162c1080a0decf4
Brian Adams <service@contrasting.org>
2022-12-01 21:13:08 -0600

    feat: add crossplane-workspaces helm chart

--- aws/crossplane.tf
+++ aws/crossplane.tf
@@ -3,7 +3,7 @@ module "aws_provider_irsa" {
   create_kubernetes_namespace       = false
   create_kubernetes_service_account = true 
   kubernetes_namespace              = "crossplane-system" 
-  kubernetes_service_account        = "crossplane-provider-terraform-6f2df84a9c2f"
+  kubernetes_service_account        = "crossplane-config"
   irsa_iam_policies                 = [aws_iam_policy.aws_provider.arn]
   # [aws_iam_policy.aws_provider.arn]
   irsa_iam_role_path                = var.irsa_iam_role_path
