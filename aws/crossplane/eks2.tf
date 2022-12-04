a5498b251360a62d5a11ed325727e585b0c79570
Brian Adams <service@contrasting.org>
2022-12-03 22:43:53 -0600

    fix: create igw for vpc

---
+++ aws/crossplane/eks2.tf
@@ -0,0 +1,99 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"
+
+  cluster_name    = "TMP"
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  enable_irsa = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = flatten([module.vpc.private_subnets, module.vpc.public_subnets])
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    disk_size      = 50
+    instance_types = ["t3.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true 
+  # create_aws_auth_configmap = true
+
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
+
+  node_security_group_additional_rules = merge( # {
+    local.ingress_rules,
+    local.egress_rules
+  )
+  /*
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  } 
+  */
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::233510574809:user/azuredevops"
+      username = "azuredevops"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "233510574809",
+  ]
+
+  tags = {
+    Environment = "prod"
+    Terraform   = "true"
+  }
+}
