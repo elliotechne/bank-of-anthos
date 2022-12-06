1490d4038e57b3105a60871a48dfc5c6f4868be2
Brian Adams <service@contrasting.org>
2022-12-06 13:43:35 -0600

    fix: increase eks instance type

--- aws/eks.tf
+++ aws/eks.tf
@@ -46,9 +46,28 @@ module "eks" {
 
   iam_role_additional_policies = {
     # AmazonEFSReadWriteMount = "arn:aws:iam::aws:policy/AmazonElasticFileSystemClientReadWriteAccess"
-    "AmazonEC2ContainerRegistryReadOnly" = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    additional                         = aws_iam_policy.node_additional.arn
   }
 
+
+resource "aws_iam_policy" "node_additional" {
+  name        = "eks-additional"
+  description = "Example usage of node additional policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+
+}
   cluster_security_group_additional_rules = {
     egress_nodes_ephemeral_ports_tcp = {
       description                = "To node 1025-65535"
