447f0f9e090312c05e2d4431ac70cc7632d5ed1e
Brian Adams <service@contrasting.org>
2022-12-06 16:54:22 -0600

    fix: increase eks instance type

--- aws/efs.tf
+++ aws/efs.tf
@@ -1,21 +1,7 @@
-resource "kubernetes_csi_driver" "efs" {
-  provider = kubernetes
-  metadata {
-    name = "efs.csi.aws.com"
-  }
-
-  spec {
-    attach_required        = false
-    volume_lifecycle_modes = [
-      "Persistent"
-    ]
-  }
-}
-
 module "efs" {
   count  = 0
   source = "cloudposse/efs/aws"
-  depends_on = [kubernetes_csi_driver.efs]
+  depends_on = [helm_release.efs]
   # Cloud Posse recommends pinning every module to a specific version
   version     = "0.32.7"
 
