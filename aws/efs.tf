dffdf9c4f512854bdb8a1579d6406950f389db00
Brian Adams <service@contrasting.org>
2022-12-06 18:54:31 -0600

    fix: efs perms

--- aws/efs.tf
+++ aws/efs.tf
@@ -10,7 +10,7 @@ resource "kubernetes_persistent_volume" "tmp" {
     access_modes = ["ReadWriteMany"]
     persistent_volume_source {
       csi {
-        driver = "efs.csi.aws.com"
+        driver        = "efs.csi.aws.com"
         volume_handle = module.efs[0].id
       }
     }
@@ -26,19 +26,19 @@ resource "kubernetes_storage_class" "efs" {
   reclaim_policy      = "Retain"
   parameters = {
     provisioningMode = "efs-ap"
-    fileSystemId = module.efs[0].id
-    directoryPerms = "700"
-    basePath = "/mnt/efs"
+    fileSystemId     = module.efs[0].id
+    directoryPerms   = "700"
+    basePath         = "/mnt/efs"
   }
   mount_options = ["tls"]
 }
 
 module "efs" {
-  count  = 1
-  source = "cloudposse/efs/aws"
+  count      = 1
+  source     = "cloudposse/efs/aws"
   depends_on = [helm_release.efs]
   # Cloud Posse recommends pinning every module to a specific version
-  version     = "0.32.7"
+  version = "0.32.7"
 
   namespace = "default"
   stage     = "prod"
@@ -48,7 +48,7 @@ module "efs" {
   subnets   = module.vpc.public_subnets
   zone_id   = [aws_route53_zone.wayofthesys.zone_id]
 
-  allowed_security_group_ids = [module.eks.cluster_security_group_id]
+  allowed_security_group_ids = [module.eks.cluster_security_group_id, module.eks.node_security_group_id]
 }
 
 resource "aws_efs_file_system_policy" "policy" {
