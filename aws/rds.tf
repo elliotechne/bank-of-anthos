a0a2554f4ae6a98f5292d3d8f0822278a89b1490
Brian Adams <service@contrasting.org>
2022-12-07 14:23:26 -0600

    feat: add aurora db cluster

--- aws/rds.tf
+++ aws/rds.tf
@@ -1,19 +1,22 @@
+resource "aws_rds_global_cluster" "bsee" {
+  global_cluster_identifier = "bsee"
+  engine                    = "aurora-postgresql"
+  engine_version            = "14.2"
+  database_name             = "bsee"
+  storage_encrypted         = true
+}
+
 module "cluster" {
   source  = "terraform-aws-modules/rds-aurora/aws"
 
-  name           = "aurora-db-postgres142"
-  engine         = "aurora-postgresql"
-  engine_version = "14.2"
+  name           = "bsee"
+  engine         = aws_rds_global_cluster.bsee.engine
+  database_name  = aws_rds_global_cluster.bsee.database_name
+  engine_version = aws_rds_global_cluster.bsee.engine_version
   instance_class = "db.r6g.large"
-  instances = {
-    one = {}
-    2 = {
-      instance_class = "db.r6g.2xlarge"
-    }
-  }
+  instances = { for i in range(2) : i => {} }
 
   vpc_id  = module.vpc.vpc_id
-  subnets = [module.vpc.database_subnet_group]
 
   allowed_security_groups = [module.security_group.security_group_id]
   allowed_cidr_blocks     = ["10.0.0.0/16"]
@@ -24,78 +27,13 @@ module "cluster" {
   apply_immediately   = true
   monitoring_interval = 10
 
-  db_parameter_group_name         = "aurouraPg"
-  db_cluster_parameter_group_name = "auroraPg"
-
-  enabled_cloudwatch_logs_exports = ["postgresql"]
+  db_subnet_group_name   = module.primary_vpc.database_subnet_group_name
+  create_db_subnet_group = false
+  create_security_group  = true
 
   tags = {
     Environment = "dev"
     Terraform   = "true"
-  }
-}
-
-module "db" {
-  count = 0
-  source = "terraform-aws-modules/rds/aws"
-
-  identifier = "bsee"
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "14.2"
-  family               = "postgres14" # DB parameter group
-  major_engine_version = "14"         # DB option group
-  instance_class       = "db.t4g.large"
-
-  allocated_storage     = 50
-  max_allocated_storage = 100
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name  = "bsee"
-  username = "bsee"
-  password = var.rds_bsee_password
-  port     = 5432
-
-  multi_az               = true
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-  create_cloudwatch_log_group     = true
-
-  backup_retention_period = 1
-  skip_final_snapshot     = true
-  deletion_protection     = false
-
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  create_monitoring_role                = true
-  monitoring_interval                   = 60
-  monitoring_role_name                  = "example-monitoring-role-name"
-  monitoring_role_use_name_prefix       = true
-  monitoring_role_description           = "Description for monitoring role"
-
-  parameters = [
-    {
-      name  = "autovacuum"
-      value = 1
-    },
-    {
-      name  = "client_encoding"
-      value = "utf8"
-    }
-  ]
-
-  db_option_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_parameter_group_tags = {
-    "Sensitive" = "low"
   }
 }
 
