74ada25c20f9b64df86b991f6b35accbfe558b5b
Brian Adams <service@contrasting.org>
2022-12-08 14:54:44 -0600

    fix: crossplane: eks cluster auth

--- aws/crossplane/helm.tf
+++ aws/crossplane/helm.tf
@@ -3,7 +3,7 @@ resource "helm_release" "aws-auth-operator" {
   provider  = helm
   name      = "aws-auth-operator"
   chart     = "charts/aws-auth-operator/chart"
-  namespace = "kube-system-system"
+  namespace = "kube-system"
   depends_on = [
     module.eks
   ]
