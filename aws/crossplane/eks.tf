d0826b4bd311e4393febd04cbf33e377851ff41f
Brian Adams <service@contrasting.org>
2022-12-08 12:53:56 -0600

    fix: manage aws config map

--- aws/crossplane/eks.tf
+++ aws/crossplane/eks.tf
@@ -30,10 +30,9 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    blue = {}
     green = {
-      min_size     = 2
-      max_size     = 10
+      min_size     = 1 
+      max_size     = 2
       desired_size = 2
 
       instance_types = ["t3.large"]
