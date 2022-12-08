5eb6c5d90a8436f6aafb66479f8fbf8f50366d5d
Brian Adams <service@contrasting.org>
2022-12-08 14:26:51 -0600

    fix: remove bsee helm chart

--- aws/crossplane/main.tf
+++ aws/crossplane/main.tf
@@ -11,10 +11,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
