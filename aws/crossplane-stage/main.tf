6edb7ba06e1ce16681dffa03418e2a784feb0cf0
Brian Adams <service@contrasting.org>
2022-12-12 10:41:50 -0600

    fix: pipeline cleanup

--- aws/crossplane-stage/main.tf
+++ aws/crossplane-stage/main.tf
@@ -20,14 +20,14 @@ data "aws_eks_cluster_auth" "boa" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.boa.cluster_endpoint
+  host                   = data.aws_eks_cluster.boa.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.boa.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.boa.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.boa.cluster_endpoint
+    host                   = data.aws_eks_cluster.boa.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.boa.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.boa.token
   }
