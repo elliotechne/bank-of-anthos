fb6dc063746fcb9ec1b55343474280938cc0b362
Brian Adams <service@contrasting.org>
2022-12-11 20:54:17 -0600

    fix: migrate crossplane to s3 backend

--- aws/helm.tf
+++ aws/helm.tf
@@ -106,7 +106,7 @@ resource "helm_release" "crossplane-terraform" {
   depends_on = [kubernetes_namespace.crossplane-system]
   name       = "crossplane-terraform"
   chart      = "charts/crossplane-terraform"
-  version    = "0.0.9"
+  version    = "0.0.10"
   namespace  = "crossplane-system"
   timeout    = 120
 
