0f84fa2f8e16af427d03029d10bf44ff5dce5fc0
Brian Adams <service@contrasting.org>
2023-05-02 17:55:41 -0500

    fix: remove argocd

--- aws/main.tf
+++ aws/main.tf
@@ -4,37 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.45"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.6.0"
-    }
   }
 }
 
 provider "aws" {
   region = var.region
-}
-
-data "aws_eks_cluster" "default" {
-  depends_on = [module.eks]
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  depends_on = [module.eks]
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.default.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.default.token
-  }
 }
