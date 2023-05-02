d2d74797e5a4db9a44a964ab773a5c0849b3289e
Brian Adams <service@contrasting.org>
2023-05-02 18:17:28 -0500

    fix: remove argocd

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -8,7 +8,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   # database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.223.0/24"]
-
+  # VERIFY IRL ENVIRONMENT
+  map_public_ip_on_launch = true 
   enable_nat_gateway = true 
   enable_vpn_gateway = true
   create_igw         = true
