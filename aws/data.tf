ffc9de64640f620079c993cb53167843a280d2d4
Brian Adams <service@contrasting.org>
2023-05-20 11:50:01 -0500

    fix: add private subnets to eks cluster

--- aws/data.tf
+++ aws/data.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "default" {
-  name = var.eks_cluster_name
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "default" {
