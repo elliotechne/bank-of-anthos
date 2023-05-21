19c9c7c5fcba5641f4dc49735d24d6e6a54d27f7
Brian Adams <service@contrasting.org>
2023-05-21 14:56:37 -0500

    fix: add private subnets to eks cluster

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -5,12 +5,12 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  public_subnets  = ["10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
   # database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.223.0/24"]
   # VERIFY IRL ENVIRONMENT
   map_public_ip_on_launch = true 
-  enable_nat_gateway = true 
+  enable_nat_gateway = false 
   enable_vpn_gateway = true
   create_igw         = true
 
