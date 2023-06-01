fb57ae0aedfe021c115cab21eb8837a89a6df212
Brian Adams <service@contrasting.org>
2023-06-01 12:19:41 -0500

    fix: re-add virtualservice and gateway for argo

---
+++ aws/helm/virtualservice.tf
@@ -0,0 +1,41 @@
+resource "kubernetes_manifest" "argocd_virtualservice" {
+  provider   = kubernetes
+  depends_on = [helm_release.argocd]
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1beta1"
+    "kind"       = "VirtualService"
+    "metadata" = {
+      "name"      = "argocd"
+      "namespace" = "argocd"
+    }
+    "spec" = {
+      "gateways" = [
+        "argocd",
+      ]
+      "hosts" = [
+        "argocd.${var.domain_name[0]}",
+      ]
+      "http" = [
+        {
+          "match" = [
+            {
+              "uri" = {
+                "prefix" = "/"
+              }
+            },
+          ]
+          "route" = [
+            {
+              "destination" = {
+                "host" = "argocd-server"
+                "port" = {
+                  "number" = 80
+                }
+              }
+            },
+          ]
+        },
+      ]
+    }
+  }
+}
