66f1d19c8326c8e867eaae1e2cee6a1e5351749a
Brian Adams <service@contrasting.org>
2023-06-03 11:50:55 -0500

    fix: crossplane cluster

--- aws/helm/crossplane.tf
+++ aws/helm/crossplane.tf
@@ -14,6 +14,7 @@ resource "helm_release" "crossplane-aws" {
 }
 
 resource "helm_release" "crossplane-terraform-install" {
+  count      = 0
   provider   = helm
   depends_on = [helm_release.crossplane-aws]
   name       = "crossplane-terraform"
@@ -29,6 +30,7 @@ resource "helm_release" "crossplane-terraform-install" {
 }
 
 resource "helm_release" "crossplane-terraform-config" {
+  count      = 0
   provider   = helm
   depends_on = [helm_release.crossplane-terraform-install]
   name       = "crossplane-terraform-config"
