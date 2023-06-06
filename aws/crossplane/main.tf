2e916f5b72604a9339609a75c9361ce8c85941cb
Brian Adams <service@contrasting.org>
2023-06-06 18:49:05 -0500

    fix: crossplane cluster

--- aws/crossplane/main.tf
+++ aws/crossplane/main.tf
@@ -4,28 +4,34 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.45"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.6.0"
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.18.1"
     }
   }
 }
 
-data "aws_eks_cluster_auth" "boa" {
-  name = "BOA-${local.environment}"
+provider "aws" {
+  region = var.region
 }
+
+/*
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
+
+*/
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
+  host                   =  module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.boa.token
-}
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.boa.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
-
