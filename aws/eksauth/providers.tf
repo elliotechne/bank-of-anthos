6ce2855d6423e083f0e17312d1bcf50eccac4221
Brian Adams <service@contrasting.org>
2023-06-07 14:36:41 -0500

    fix: decouple eks auth in pipeline

--- aws/eksauth/providers.tf
+++ aws/eksauth/providers.tf
@@ -11,3 +11,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.default.token
 }
+
+provider "aws" {
+  region = var.region
+}
