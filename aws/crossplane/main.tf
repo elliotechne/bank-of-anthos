abe1dfeb7ea1e2366ce4b443c562c3c114a8e325
Brian Adams <service@contrasting.org>
2023-06-06 21:35:08 -0500

    change kubectl provider to data source

--- aws/crossplane/main.tf
+++ aws/crossplane/main.tf
@@ -11,11 +11,11 @@ terraform {
   }
 }
 
-data "aws_eks_cluster" "bookpeople" {
+data "aws_eks_cluster" "default" {
   name = module.eks.cluster_name
 }
 
-data "aws_eks_cluster_auth" "bookpeople" {
+data "aws_eks_cluster_auth" "default" {
   name = module.eks.cluster_name
 }
 
