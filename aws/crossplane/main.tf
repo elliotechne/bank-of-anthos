f663ebc1254985c7591e480d4498e1f40500fbdb
Brian Adams <service@contrasting.org>
2023-06-06 19:52:31 -0500

    fix: crossplane cluster

--- aws/crossplane/main.tf
+++ aws/crossplane/main.tf
@@ -11,19 +11,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
-
-/*
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
-}
-
-*/
-
 provider "kubernetes" {
   host                   =  module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
