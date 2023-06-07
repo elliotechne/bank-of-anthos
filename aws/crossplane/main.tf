ecc86dea23e46fe7c348b53b08a85039a349beab
Brian Adams <service@contrasting.org>
2023-06-06 21:31:25 -0500

    change kubectl provider to data source

--- aws/crossplane/main.tf
+++ aws/crossplane/main.tf
@@ -11,14 +11,16 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  host                   =  module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+data "aws_eks_cluster" "bookpeople" {
+  name = module.eks.cluster_name
+}
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+data "aws_eks_cluster_auth" "bookpeople" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
 }
