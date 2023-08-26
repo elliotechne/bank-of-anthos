7b3f29d3eeb5de2bef4ccdf2ad3f2bf54b5fc3b6
Brian Adams <service@contrasting.org>
2023-08-26 18:26:56 -0400

    fix: configure kubernetes provider

---
+++ do/pulumi/kubernetes/index.ts
@@ -0,0 +1,57 @@
+import * as pulumi from "@pulumi/pulumi"; 
+import * as digitalocean from "@pulumi/digitalocean";
+import * as kubernetes from "@pulumi/kubernetes"; 
+
+const cluster = new digitalocean.KubernetesCluster("boa", {
+    region: digitalocean.Region.SFO2,
+    version: "1.27.4-do.0",
+    nodePool: {
+        name: "worker01",
+        size: digitalocean.DropletSlug.DropletS2VCPU2GB,
+        nodeCount: 3,
+    },
+});
+export const kubeconfig = cluster.kubeConfigs[0].rawConfig;
+
+const provider = new kubernetes.Provider("do-k8s", { kubeconfig })
+
+const config = new pulumi.Config();
+const k8sNamespace = config.get("k8sNamespace") || "default";
+const appLabels = {
+  app: "nginx-ingress",
+}
+
+// Create NS
+const ingressNs = new kubernetes.core.v1.Namespace("ingressns", {metadata: {
+    labels: appLabels,
+    name: k8sNamespace
+}}, { provider });
+
+// Use Helm to install the Nginx ingress controller
+const ingressController = new kubernetes.helm.v3.Release("ingresscontroller", {
+    chart: "nginx-ingress",
+    namespace: ingressNs.metadata.name,
+    repositoryOpts: {
+        repo: "https://helm.nginx.com/stable",
+    },
+    skipCrds: true,
+    values: {
+        controller: {
+            enableCustomResources: false,
+            appprotect: {
+                enable: false,
+            },
+            appprotectdos: {
+                enable: false,
+            },
+            service: {
+                extraLabels: appLabels,
+            },
+        },
+    },
+    version: "0.14.1",
+},{provider});
+
+// Export some values for use elsewhere
+export const name = ingressController.name;
+
