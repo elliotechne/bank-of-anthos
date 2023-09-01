01a85c1d0a300d037c68fcff737f2da7d292baac
Brian Adams <service@contrasting.org>
2023-09-01 19:16:03 -0400

    fix: remove awscli install via tf

--- aws/main.tf
+++ aws/main.tf
@@ -21,7 +21,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.default.token
 }
-
 */
 
 provider "kubernetes" {
