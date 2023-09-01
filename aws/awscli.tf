d5a36109fe8d0f8636286f9042ba5ada17c14ba9
Brian Adams <service@contrasting.org>
2023-09-01 13:03:40 -0400

    fix: install awscli via terraform

--- aws/awscli.tf
+++ aws/awscli.tf
@@ -2,12 +2,12 @@ resource "terraform_data" "awscli" {
   provisioner "local-exec" {
     command = <<EOF
 set -e
-WORKDIR=/tmp/${uuid()}
+WORKDIR=/tmp/aws
 mkdir -p "$WORKDIR"
 cd "$WORKDIR"
 curl -f "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
-./awscli-bundle/install -i "$WORKDIR"/aws
+./awscli-bundle/install // -i "$WORKDIR"/aws
 EOF
   interpreter = ["bash"]
   }
