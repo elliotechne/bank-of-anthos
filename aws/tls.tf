32165bb2904a962491e7583e6f54324edf6616dd
Brian Adams <service@contrasting.org>
2023-09-06 12:53:07 -0400

    fix: remove key_algorithm for ca

--- aws/tls.tf
+++ aws/tls.tf
@@ -52,7 +52,7 @@ resource "tls_private_key" "key" {
 }
 
 resource "tls_cert_request" "request" {
-  key_algorithm   = tls_private_key.key.algorithm
+  // key_algorithm   = tls_private_key.key.algorithm
   private_key_pem = tls_private_key.key.private_key_pem
 
   dns_names = [
