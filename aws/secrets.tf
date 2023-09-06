24bcd14d04c5b0dbec4195253f24ea37ee417a8e
Brian Adams <service@contrasting.org>
2023-09-06 12:23:39 -0400

    fix: added tls secrets for zerossl

--- aws/secrets.tf
+++ aws/secrets.tf
@@ -1,8 +1,3 @@
-resource "random_string" "random" {
-  length           = 16
-  special          = true
-}
-
 resource "kubernetes_secret" "eab_hmac" {
   metadata {
     name      = "sslcom-hmac-key"
@@ -10,24 +5,7 @@ resource "kubernetes_secret" "eab_hmac" {
   }
 
   data = {
-    secret = var.sslcom_hmac_key
-  }
-
-  type = "kubernetes.io/opaque"
-}
-
-resource "kubernetes_secret" "oath_proxy_secret" {
-  depends_on = [kubernetes_namespace.oauth_proxy]
-  for_each = toset(var.domain_name)
-  metadata {
-    name      = "${replace(each.key, ".", "-")}-oauth-proxy-tls"
-    namespace = "oauth-proxy"
-  }
-
-  data = {
-      github-client-id = base64encode(var.oauth_client_id)
-      github-client-secret: base64encode(var.oauth_client_secret)
-      cookie-secret: "V1pBekJxd05PVjIrc1lrai9nSGdYZz09" 
+    secret = var.zerossl_eab_hmac_key
   }
 
   type = "kubernetes.io/opaque"
