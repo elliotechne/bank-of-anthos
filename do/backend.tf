49e30e71e7aea5976efc0a044f34c815523e3c61
elliotechne <elliotechne42@gmail.com>
2025-08-31 14:12:42 -0400

    Update backend.tf to new format for 1.13.1

--- do/backend.tf
+++ do/backend.tf
@@ -1,10 +1,12 @@
 terraform {
   backend "s3" {
-    endpoint.s3                 = "nyc3.digitaloceanspaces.com"
-    key                         = "atlantis.tfstate"
     bucket                      = "bankofanthos"
-    region                      = "us-east-1"
+    key                         = "atlantis.tfstate"
+    region                      = "us-east-1"  # Required but ignored by DO Spaces
+    endpoint                    = "https://nyc3.digitaloceanspaces.com"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
   }
 }
