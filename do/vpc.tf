deded6d1db6051420f2b2515c9fed4e845f94639
elliotechne <elliotechne42@gmail.com>
2025-08-31 13:57:37 -0400

    Update vpc.tf - use vpc module

--- do/vpc.tf
+++ do/vpc.tf
@@ -1,5 +1,8 @@
-resource "digitalocean_vpc" "production" {
-  name     = "boa"
-  region   = "nyc3"
-  ip_range = "10.10.10.0/24"
+module "vpc" {
+  source      = "terraform-do-modules/vpc/digitalocean"
+  version     = "1.0.0"
+  name        = local.name
+  environment = local.environment
+  region      = local.region
+  ip_range    = "10.0.0.0/16"
 }
