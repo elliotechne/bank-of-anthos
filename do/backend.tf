d756f2f4d385ebaa54fe0391d6b0c26c9f8fe0e5
elliotechne <elliotechne42@gmail.com>
2025-08-31 13:30:24 -0400

    Update backend.tf to use extended name for bucket

--- do/backend.tf
+++ do/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     endpoint                    = "nyc3.digitaloceanspaces.com"
     key                         = "atlantis.tfstate"
-    bucket                      = "boa"
+    bucket                      = "bankofanthos"
     region                      = "us-east-1"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
