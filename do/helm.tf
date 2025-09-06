027446e827123eadba1814f517a2ca93a3062ca8
elliotechne <elliotechne42@gmail.com>
2025-09-06 13:46:29 -0400

    fix: version string

--- do/helm.tf
+++ do/helm.tf
@@ -13,7 +13,10 @@ resource "helm_release" "external-dns" {
   name       = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns/" 
   chart      = "external-dns"
+  namespace  = "cert-manager"
   version    = "1.18.0"
+  depends_on = [module.cluster, 
+                kubernetes_namespace.cert-manager]
 
       values = [<<EOF
 provider:
