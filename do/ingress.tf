044d3eabb21e7a0dd00d518122a72a229d3e392e
elliotechne <elliotechne42@gmail.com>
2025-09-07 13:35:12 -0400

    feat: added frontend boa to root domain

--- do/ingress.tf
+++ do/ingress.tf
@@ -51,3 +51,56 @@ resource "kubernetes_ingress_v1" "argocd" {
   }
 }
 
+resource "kubernetes_ingress_v1" "boa" {
+  depends_on = [
+    helm_release.boa,
+    helm_release.cluster-issuer
+  ]
+  for_each = toset(var.domain_name)
+  metadata {
+    name      = "${each.key}-frontend-ingress"
+    namespace = "boa"
+    annotations = {
+      "kubernetes.io/ingress.class"          = "nginx"
+      "ingress.kubernetes.io/rewrite-target" = "/"
+      "cert-manager.io/cluster-issuer"       = "zerossl"
+    }
+  }
+  spec {
+    default_backend {
+      service {
+        name = "frontend"
+        port {
+          number = 80
+        }
+      }
+    }
+    dynamic "rule" {
+      for_each = toset(var.domain_name)
+      content {
+        host = "${rule.value}"
+        http {
+          path {
+            backend {
+              service {
+                name = "frontend"
+                port {
+                  number = 80
+                }
+              }
+            }
+            path = "/"
+          }
+        }
+      }
+    }
+    dynamic "tls" {
+      for_each = toset(var.domain_name)
+      content {
+        secret_name = "boa-tls"
+        hosts       = ["${tls.value}"]
+      }
+    }
+  }
+}
+
